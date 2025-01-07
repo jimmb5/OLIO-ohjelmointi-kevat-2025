cd428e1097dfc347499450aa48a84282417f8ade
jimmb5 <jimmy.bergbacka@gmail.com>
2025-01-07 12:12:43 +0200

    Ensimmainen harjoitus tehty

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+int game (int maxnum){
+    std::srand(static_cast<unsigned int>(std::time(0))); // netistä katsottu, ilman tätä arpoo aina saman numeron
+    int satunnaisluku = std::rand() % maxnum + 1;
+    int arvaus;
+    bool endgame = false;
+    int arvaukset = 0;
+
+    while(endgame==false){
+        std:: cout <<"Arvaa luku: " << endl;
+        std:: cin >> arvaus;
+        arvaukset++;
+
+        if (arvaus < satunnaisluku){
+            std::cout << "luku on suurempi" << endl;
+        }
+        else if (arvaus > satunnaisluku){
+            std::cout << "luku on pienempi" << endl;
+
+        }
+        else if (arvaus == satunnaisluku){
+            std::cout << "oikein" << endl;
+            std::cout << "arvausten lkm: " << arvaukset <<  endl;
+
+            endgame=true;
+        }
+    }
+    return arvaukset;
+}
+
 
 int main()
 {
-    printf("Hell world %d\n",5);
-    std::cout << "Hello World!" << 10 << std::endl;
+    int maxnum;
+    std::cout << "syota max luku: " << endl;
+    std::cin >> maxnum;
+
+    int arvaukset = game(maxnum);
+
+    std::cout << "Peli paattyi, arvaukset: " << arvaukset << endl;
     return 0;
 }
+
